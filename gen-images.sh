8b6aa02816910082ece273f0fc765e3ba7144847
Jan Skoda <jan.skoda@liquiditylabs.xyz>
2026-02-16 21:28:43 +0100

    fix drawing

--- gen-images.sh
+++ gen-images.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 keymap -c ./keymap_drawer.config.yaml parse -z ./config/kyria_rev3.keymap > draw/kyria_rev3.yaml
-keymap -c ./keymap_drawer.config.yaml draw draw/kyria_rev3.yaml -k "kyria/rev3" -l "LAYOUT_kyria" --keys-only > ./draw/kyria_rev3.svg
-keymap -c ./keymap_drawer.config.yaml draw draw/kyria_rev3.yaml -k "kyria/rev3" -l "LAYOUT_kyria" -s base --combos-only > ./draw/combos.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/kyria_rev3.yaml -k "splitkb/kyria/rev3" -l "LAYOUT_kyria" --keys-only > ./draw/kyria_rev3.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/kyria_rev3.yaml -k "splitkb/kyria/rev3" -l "LAYOUT_kyria" -s base --combos-only > ./draw/combos.svg
